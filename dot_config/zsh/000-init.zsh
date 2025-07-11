58ebae83d634d3c6029616d62898bf64069c78dd
usee <useekaw@gmail.com>
2025-07-11 17:24:31 +0200

    Fix syntax

--- dot_config/zsh/000-init.zsh
+++ dot_config/zsh/000-init.zsh
@@ -1,6 +1,6 @@
 # initialize plugin manager
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
-if [ ! -d "${ZINIT_HOME}" ]; the
+if [ ! -d "${ZINIT_HOME}" ]; then
     mkdir -p "$(dirname ${ZINIT_HOME})"
     git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}"
 fi
