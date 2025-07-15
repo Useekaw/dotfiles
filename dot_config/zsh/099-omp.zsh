d0a9098844f3a00b88214e6832a19306e5e0b06d
Useekaw <useekaw@gmail.com>
2025-07-15 08:15:46 +0000

    Update oh my posh theme

--- dot_config/zsh/099-omp.zsh
+++ dot_config/zsh/099-omp.zsh
@@ -1,5 +1,5 @@
 # oh-my-posh
 if [ -x "$(command -v oh-my-posh)" ]; then
-    eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/pure.omp.json)"
+    eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/onehalf.minimal.omp.json)"
 fi
 
